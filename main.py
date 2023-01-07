4ba5587c165cb3c6b57c856dacec892a4e1af278
Haxorsprogramming <haxors@uinsu.ac.id>
2023-01-07 23:39:59 +0700

    update for cpanel

--- main.py
+++ main.py
@@ -12,6 +12,14 @@ app = Flask(__name__)
 def index():
     return render_template('home.html', dr=BASE_URL)
 
+@app.route('/data-buku')
+def dataBuku():
+    return render_template('data-buku.html', dr=BASE_URL)
+
+@app.route('/data-kategori')
+def dataKategori():
+    return render_template('data-kategori.html', dr=BASE_URL)
+
 # jalankan server 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=7009)
